043840830e38eb6b6b72f1d7eb5426a1029aa023
adentsen <adentsen@gmail.com>
2020-12-07 21:35:30 +0800

    fix datetime bug

--- scheduler.py
+++ scheduler.py
@@ -1,17 +1,28 @@
 # Purpose: Enable get_price to run only during business hours
 
 import time
-from datetime import datetime
+import datetime
 import schedule
 from download_stock_price_from_bursa import get_price
+
+# get_price(company_cd  = "0002",
+#                 in_tbl      = "fin_data5",
+#                 db_host     = "localhost",
+#                 db_user     = "bursa_user",
+#                 db_password = "password",
+#                 db_database = "bursa_securities",
+#                 )
+
 
 # Only run get price if it is a weekday from 9 to 5
 def main():
   dttm = datetime.datetime.now()
   weekno = datetime.datetime.today().weekday()
+  at9am = dttm.replace(hour=9, minute=0, second=0, microsecond=0)
+  at5pm = dttm.replace(hour=17, minute=0, second=0, microsecond=0)
   if weekno<5:
-    if dttm.time() >= 9 and dttm.time() <=5:
-      get_price(company_cd  = "7106",
+    if dttm > at9am and dttm < at5pm:
+      get_price(company_cd  = "0002",
                 in_tbl      = "fin_data5",
                 db_host     = "localhost",
                 db_user     = "bursa_user",
@@ -19,8 +30,9 @@ def main():
                 db_database = "bursa_securities",
                 )
 
-schedule.every(10).minutes.do(main)
+schedule.every(1).minutes.do(main)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
